218c7faaf624b8a698c1764a2dfad510db021e2c
Kammary Sridhar <kammary_sridhar@yahoo.com>
2024-01-29 16:35:21 +0530

    Comprehension Example
    Comprehension Example

---
+++ ComprehensikonsExample.py
@@ -0,0 +1,7 @@
+list1=[2,3,48,9,0,4,6]
+evenList=[x for x in list1 if x%2==0]
+print(f"Normal List={list1} and Even List={evenList}")
+#Prepare the 2D Matrix
+
+twoDMatrix=[ [x for x in range(2)] for k in list1 ]
+print(f"2D Matrix = {twoDMatrix}")
